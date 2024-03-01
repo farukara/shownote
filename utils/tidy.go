ad55def0648ac94418c956da8de9c6b9090a04d7
farukara <69348522+farukara@users.noreply.github.com>
2024-03-01 16:33:43 +0300

    refac: some perf improvements and refactoring concurrency patterns

--- utils/tidy.go
+++ utils/tidy.go
@@ -19,11 +19,11 @@ type filecmd struct {
 // find orphan notes that does not have a corresponding task
 // in taskwarrior
 func Tidy(notes_folder, file_ext string) {
-    someFilesFound := false
     home,err := os.UserHomeDir()
     if err != nil {
         log.Err(err).Stack().Msg("could not get home dir")
         fmt.Println("could not get home dir:", err )
+        return
     }
     notes_folder = path.Join(home, notes_folder)
 
@@ -31,74 +31,115 @@ func Tidy(notes_folder, file_ext string) {
     if err != nil {
         log.Err(err).Stack().Msg("could not glob the notes folder")
         fmt.Println("could not glob the notes folder:", err )
+        return
     }
 
-
-    ch := make(chan filecmd, 1000)
-    wgout := &sync.WaitGroup{}
-    wgout.Add(2)
-    go func (ch chan filecmd) {
-        defer wgout.Done()
+    filesChan := make(chan string)
+    go func() {
         for _, file := range files {
-            //if its not a uuid named task file
-            if len(file) != 39 {
-                continue
-            }
-
-            // look if task exist for that note
-            app := "task"
-            arg0 := "_get"
-            arg1 := file[:8] + ".id"
-
-            cmd := exec.Command(app, arg0, arg1)
-            filecmd := filecmd{
-                cmd: cmd,
-                filename: file,
-            }
-            ch <- filecmd
-
+            filesChan <- file
         }
-        close(ch)
-    }(ch)
+        close(filesChan)
+    }()
+    
 
-    go func (ch chan filecmd) {
-        defer wgout.Done()
-        wg := &sync.WaitGroup{}
-        wg.Add(4)
-        for i:=0; i<4; i++ {
-            go func (ch chan filecmd) {
-                defer wg.Done()
-                for {
-                    filecmd, ok := <- ch
-                    if !ok {
-                        break
-                    }
-                    id, err := filecmd.cmd.Output()
-                    if err != nil {
-                        fmt.Println("\033[7;31merror occured during following command:")
-                        fmt.Println("filecmd")
-                        continue
-                    }
-                    if len(id) < 2 { // only 10 exist in byte slice, which is line feed
-                        fmt.Println("\033[7;34mFollowing files don't have an assosiated task.")
-                        fmt.Println("They are called Orphan Notes")
-                        fmt.Println("You can delete them with the following command:")
-                        fmt.Println("\033[7;0m")
-                        fmt.Println("rm  <filename>")
-                        fmt.Println()
-                        fmt.Println("\033[7;34mOrphan files:\033[7;0m")
-                        fmt.Println(path.Join(notes_folder, filecmd.filename))
-                        someFilesFound = true
-                        continue
-                    }
-                    // fmt.Println(path.Join(notes_folder, filecmd.filename))
+    cmdWorkers := 30
+
+    fileCmdChan := make(chan filecmd)
+    // wgout := &sync.WaitGroup{}
+    // wgout.Add(1)
+    cmdwg := &sync.WaitGroup{}
+    cmdwg.Add(cmdWorkers)
+    for i:=0; i<cmdWorkers; i++ {
+        go func (ch chan<- filecmd) {
+            defer cmdwg.Done()
+            for file := range filesChan {
+                //if its not a uuid named task file
+                if len(file) != 39 {
+                    continue
                 }
-            }(ch)
-        }
+
+                // look if task exist for that note
+                app := "task"
+                arg0 := "_get"
+                arg1 := file[:8] + ".uuid"
+
+                cmd := exec.Command(app, arg0, arg1)
+                filecmd := filecmd{
+                    cmd: cmd,
+                    filename: file,
+                }
+                // fmt.Println(" sending to chan")
+                ch <- filecmd
+
+            }
+        }(fileCmdChan)
+    }
+
+    go func () {
+        cmdwg.Wait()
+        close(fileCmdChan)
+    }()
+
+    var orphanFilesChan = make(chan string)
+
+    workers := 50
+    wg := &sync.WaitGroup{}
+    wg.Add(workers)
+
+    for i:=0; i<workers; i++ {
+        go func () {
+            defer wg.Done()
+            for filecmd := range fileCmdChan {
+                // fmt.Println("received on chan............")
+                // filecmd, ok := <- ch
+                // if !ok {
+                //     break
+                // }
+                uuid, err := filecmd.cmd.Output()
+                if err != nil {
+                    fmt.Println("\033[7;31merror occured during following command:")
+                    fmt.Println(uuid, filecmd)
+                    continue
+                }
+                if len(uuid) != 37 { // length of uuid, plus 1 for newline, not including the .md file extention
+                    orphanFilesChan <- filecmd.filename
+                }
+            }
+
+        }()
+    }
+    var orphanFiles []string
+
+    go func() {
         wg.Wait()
-    }(ch)
-    wgout.Wait()
-    if !someFilesFound {
+        close(orphanFilesChan)
+    }()
+
+    lastwg := &sync.WaitGroup{}
+    lastwg.Add(1)
+    go func () {
+        defer lastwg.Done()
+        for orphanfile := range orphanFilesChan {
+            orphanFiles = append(orphanFiles, orphanfile)
+        }
+    }()
+    lastwg.Wait()
+
+    if len(orphanFiles) == 0 {
         fmt.Println("No orphan notes have been found.")
+    } else {
+        fmt.Println("\033[7;34mFollowing files don't have an assosiated task.")
+        fmt.Println("They are called Orphan Notes")
+        fmt.Println("You can delete them with the following command:")
+        fmt.Println("\033[7;0m")
+        fmt.Println("rm  <filename>")
+        fmt.Println()
+        fmt.Println("\033[7;34mOrphan files:\033[7;0m")
+        for i,orphanFile := range orphanFiles {
+            fmt.Println(i+1, ". ", path.Join(notes_folder, orphanFile))
+
+        }
+
     }
 }
